37380930ea1b8173090b4523d8f7d80303fb9a9a
Murilo Molina <murilo.m.barone@gmail.com>
2023-10-26 21:51:25 -0300

    p_final(dart flutter): container certificados

--- portifolio/lib/pages/pagina_certificados.dart
+++ portifolio/lib/pages/pagina_certificados.dart
@@ -13,20 +13,50 @@ class PaginaCertificados extends StatelessWidget {
       body: ListView(
         children: [
           Wrap(
-            spacing: 20.0,
+            spacing: 30.0,
             runSpacing: 20.0,
             children: [
-              Container(
+            
+            Container(
             padding: const EdgeInsets.all(16), 
             decoration: BoxDecoration(
-              color: Colors.grey[200], 
+              color: const Color.fromARGB(255, 158, 153, 153), 
               borderRadius: BorderRadius.circular(12), 
             ),
             child:
-              Image.asset('lib/assets/certificados/AWS_Academy_Graduate___AWS_Academy_Cloud_Foundations_Badge20230603-28-jswue9_page-0001.jpg',width: 650,),),
-              Image.asset('lib/assets/certificados/clmsCertificate_page-0001.jpg',width: 660,),
-              Image.asset('lib/assets/certificados/CM3033-IntrodjucaoaCienciadjeDadjos-2023 (1)_page-0001.jpg',width: 660,),
-              Image.asset('lib/assets/certificados/CM4042-FundjamentosdjeComputacaoemNuvem-AWSAcadjemy-2023 (1)_page-0001.jpg',width: 660,),
+              Image.asset('lib/assets/certificados/AWS_Academy_Graduate___AWS_Academy_Cloud_Foundations_Badge20230603-28-jswue9_page-0001.jpg',width: 630,)
+            ),
+            
+            Container(
+            padding: const EdgeInsets.all(16), 
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 158, 153, 153), 
+              borderRadius: BorderRadius.circular(12), 
+            ),
+            child:  
+              Image.asset('lib/assets/certificados/clmsCertificate_page-0001.jpg',width: 630,)
+            ),
+            
+            Container(
+            padding: const EdgeInsets.all(16), 
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 158, 153, 153), 
+              borderRadius: BorderRadius.circular(12), 
+            ),
+            child:
+              Image.asset('lib/assets/certificados/CM3033-IntrodjucaoaCienciadjeDadjos-2023 (1)_page-0001.jpg',width: 630,)
+            ),
+            
+            Container(
+            padding: const EdgeInsets.all(16), 
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 158, 153, 153), 
+              borderRadius: BorderRadius.circular(12), 
+            ),
+            child:
+              Image.asset('lib/assets/certificados/CM4042-FundjamentosdjeComputacaoemNuvem-AWSAcadjemy-2023 (1)_page-0001.jpg',width: 630,)
+            ),
+
             ],
           ),
         ],
